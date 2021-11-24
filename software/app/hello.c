aca99ba3b2e080d73a62fea3a24151e971116a11
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2021-11-24 05:01:20 -0300

    acesso alguma coisa

--- software/app/hello.c
+++ software/app/hello.c
@@ -1,10 +1,35 @@
+// https://stackoverflow.com/questions/11081032/how-do-i-check-the-memory-being-used-by-my-c-code
+// https://www.hackerearth.com/practice/notes/memory-layout-of-c-program/
+//https://github.com/PabloVieira/TCS-hf-risc/blob/7fd415efa34263e4142bbbc7ddd2d9fffc674e0c/software/include/hf-risc.h
+// TCS-hf-risc/software/include/hf-risc.h
+//https://stackoverflow.com/questions/4462915/find-the-exact-address-of-variable-buf
 #include <hf-risc.h>
 
-int main(void){
-	putchar('!');
-	putchar('\n');
-	printf("hello world!\n");
+void main(void){
+	printf("helll world!\n");
+	int a = 42;
+	int b = 8;
+	int c = a+b;
+	uint64_t *ptr;
+	//ptr = 0x40000520;
+	*ptr = 0;
+	printf("%d+%d=%d \n",a,b,c);
+	printf("&prt:%0x\n", &ptr); // endereco de p
+	int i;
+	for(i=0;i<1000;i++){
+		printf("i:%d *prt:%x\n", i, *(&ptr+0x4fffffe8+i)); 
+		if(*(&ptr+0x4fffffe8+i) == 0x3200693){
+			printf("ok\n");
+			break;
+		}
+	}
 
-	return 0;
+	printf("i:%d *prt:%x\n", i, *(&ptr+0x4fffffe8+i));
+	*(&ptr+0x4fffffe8+i) = 0x3200793;
+
+	printf("i:%d *prt:%x\n", i, *(&ptr+0x4fffffe8+i));
+	printf("%d+%d=%d \n",a,b,c);
+
+
 }
 
